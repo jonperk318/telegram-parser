7dba26c0cec5f949414ef91cc8cfafe27574fa07
Jonathan Perkins <jonperk318@gmail.com>
2024-08-12 23:08:52 -0500

    Added TG parser

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# telegram-parser
-OSINT tool: Telegram chat parser to find credit/debit card information using keywords
+# Telegram Chat CC Parser
+
+This script takes in a JSON file exported from Telegram chat history and finds credit/debit card information 
+in posts with keyword mentions. This is to be used to search dumps for sensitive data so the victims
+can be made aware of the breach and the cards can be canceled. This is ONLY created to PREVENT 
+further exploitation.
+
+## How to use
+
+This script is written Python 3.12.0.
+
+1. Find a Telegram channel with credit/debit card full dumps in a format that can be copied via clicking (the "code" property has the value of the cc full).
+2. Export chat history as a JSON (machine readable) format by navigating to the top right menu of the Telegram desktop app. Deselect all boxes and change `html` to `json`.
+
+![demo.png](demo.png)
+
+3. Download `telegram-cc-parser.py` and place the script in the same directory as the `result.json` file from the Telegram chat export.
+4. Run the following command from terminal:
+
+```python
+python3 telegram-cc-parser.py result.json [keyword]
+```
+5. After a short time, the results will be exported to a `csv` file with the following format:
+
+- `from-channel-name`: channel name post originated from
+- `from-channel-id`: channel ID
+- `message-id`: unique message ID (to find exact message by appending to Telegram chat URL)
+- `timestamp`: MM/DD/YYYY 00:00
+- `bin`: first 6 digits of card number
+- `cc-number`: card number
+- `expiration`: expiration date
+- `cvv`: pin
+
+## Notes
+
+Please use responsibly! This is an open-source intelligence (OSINT) tool, and as any of these tools, it should be used to counteract criminal activity and protect victim data.
+
+I originally created this for a more narrow use case and didn't intend to share it, but I realized its potential to help a researcher/analyst that stumbles accross it. If you have any suggestions on further features, or if you encounter bugs, please open an issue. Also feel free to contact me directly!
---
+++ telegram_cc_parser.py
@@ -0,0 +1,88 @@
+"""
+TELEGRAM CHAT CC PARSER
+
+OSINT tool by Jonathan Perkins
+contact at jonperk318@gmail.com for questions/bugs
+
+This script takes in a JSON file exported from Telegram chat history and finds credit/debit card information 
+in posts with keyword mentions. This is to be used to search dumps for sensitive data so the victims
+can be made aware of the breach and the cards can be canceled. This is ONLY created to PREVENT 
+further exploitation.
+"""
+
+
+import json
+import csv
+
+file = input("Enter name of JSON file (excluding extension):\n")
+
+f = open(("./" + file + ".json"), encoding="utf8")
+data = json.loads(f.read())
+f.close()
+
+channel_id = str(data["id"])
+
+keyword = input("Enter search keyword (bank/institution name, etc.) (case insensitive):\n")
+
+def process_message(message):
+
+    if message["type"] != "message":
+        return None
+    
+    message_id = str(message["id"])
+    timestamp = message["date"].replace("T", " ")
+    from_chat = message["from"]
+    from_id = message["from_id"]
+    content = message.get("text", "")
+
+    if isinstance(content, list):
+
+        for text in content:
+            if isinstance(text, dict):
+                if text["type"] == "bold" or text["type"] == "plain":
+                    if keyword in text["text"].lower():
+                        for i in content:
+                            if isinstance(i, dict):
+                                if i["type"] == "code":
+                                    ccs = i["text"]
+                                    ccs = ccs.split("\n")
+                                    for cc in ccs:
+                                        cc = cc.split("|")
+                                        cc_number = cc[0]
+                                        exp = ("" + cc[1] + "/" + cc[2])
+                                        cvv = cc[3]
+                                        return {
+                                                "from-channel-name": from_chat,
+                                                "from-channel-id": from_id.removeprefix('channel'),
+                                                "message-id": message_id,
+                                                "timestamp": timestamp,
+                                                "bin": cc_number[:6], 
+                                                "cc-number": cc_number,
+                                                "expiration": exp,
+                                                "cvv": cvv
+                                                }
+        
+    return None
+
+
+with open("./" + keyword + " - " + channel_id + ".csv", "w", encoding="utf-8-sig", newline="") as output_file:
+
+    writer = csv.DictWriter(output_file, [
+                                        "from-channel-name",
+                                        "from-channel-id",
+                                        "message-id",
+                                        "timestamp",
+                                        "bin",
+                                        "cc-number",
+                                        "expiration",
+                                        "cvv"
+                                          ], dialect="unix", quoting=csv.QUOTE_NONNUMERIC)
+    writer.writeheader()
+
+    for message in data["messages"]:
+        row = process_message(message)
+        if row is not None:
+            writer.writerow(row)
+    
+
+print("Done")
